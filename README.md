4cd3d23203ceff0d2f1a49b8f913d8fd0de600c6
Barna Varga <barnavarga@invenshure.com>
2016-09-19 18:57:02 +0200

    first demo release

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # js-browsercheck
-Checks js scripts for browser incompatibilties
+
+## Description
+Checks js scripts for browser incompatibilties. Uses (jscc)[http://jscc.info/] to check all js scripts and then stores to analysed data in a text file. The website and single file checker was created by (tbusser)[https://github.com/tbusser/jscc]. The app uses selenium to check which browsers are not compatible with your js scripts and then sums up the outputs (in which file with which function which browsers are not compatible)
+
+## Installation
+### Pre-requirements
+- Selenium  
+- Python3  
+- Chrome and chromedriver
+
+### After Pre-requirements are met do the following steps:
+1. Go to location you downloaded or cloned the git repo to  
+2. type `python3 own_js_tester.py /where/js/files/are/located`
+
+## Tests
+No tests where done atm
+
+##TO-DO
+- Better result output.
+- Error Handling
+
+##Release notes
+n/a
+
+## Licences
+Please check description for the persons git page and website for the js checker I used.
--- own_js_tester.py
+++ own_js_tester.py
@@ -2,6 +2,9 @@ import argparse
 import subprocess
 import time
 import os
+import json
+# import pprint
+import datetime
 
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
@@ -10,15 +13,49 @@ from selenium.webdriver.common.keys import Keys
 
 #config
 URL = 'http://jscc.info/'
-RESULTS = '/home/barnav/temp/'
+APP_MAIN_FOLDER = os.path.expanduser('~/js-browser/')
+
+def make_cwf():
+    '''
+        Creates a folder from the current time as a continuous string without ms
+        Returns: The name of the folder
+    '''
+    time = str(datetime.datetime.now())
+    time = time.split(".")[0]
+    time_date = time.split(" ")[0]
+    time_hms = time.split(" ")[1]
+    time_date_clumped = time_date.split("-")
+    time_date_clumped = ("").join(time_date_clumped)
+    time_hms_clumped = time_hms.split(":")
+    time_hms_clumped = ("").join(time_hms_clumped)
+    folder_name = time_date_clumped+time_hms_clumped+"/"
+    return folder_name
+
+def create_result_folder(location):
+    '''
+        Checks if main app folder exists and if not make it and after create cw folder
+        Args: Where to create result folder
+        Resturns: Full path of folder
+    '''
+    timestamp = make_cwf()
+    if not os.path.exists(location):
+        os.mkdir(location)
+    if location.endswith('/'):
+        location = location
+    else:
+        location = location+"/"
+    cwd = location+timestamp
+    if not os.path.exists(cwd):
+        make_timestamp_folder = os.mkdir(cwd)
+    return cwd
 
 def check_js_files(filepath):
     '''
-        Goes through all files and dirs under given root and saves all javascript files and their path to a list variable
+        Goes through all files and dirs under given root and saves all javascript files and their path to a list
         Args: the root dir
         Returns: a list of all the found js files with location (format: '/home/user/given/path/foundjsfile.js')
     '''
-    js_files_path = []
+    js_files = []
     for root, dirs, files in os.walk(filepath, topdown=True, followlinks=False):
         for name in files:
             if name.lower().endswith('.js'):
@@ -26,12 +63,25 @@ def check_js_files(filepath):
                     path = '/'.join((root, name))
                 else:
                     path = "".join((root, name))
-                js_files_path.append(path)
-    return js_files_path
+                js_files.append(path)
+    return js_files
 
-def filename(js_file):
+def filename(js_file, make_json):
+    '''
+        Gets the name of the file and removes all 'junk' and makes a JSON file (ie.: path and extension)
+        /home/user/test/to/given/path/test.js will become test.JSON
+        Args: full path to file with filename and extension
+        Returns: The name of the file as a JSON file
+    '''
     filename = js_file.split("/")
-    return filename[-1]
+    filename = filename[-1].split(".")
+    filename.pop(-1)
+    filename = (".").join(filename)
+    if make_json == "Yes":
+        filename = filename+".JSON"
+    else:
+         filename = filename
+    return filename
 
 def file_open_and_copy_text_function(js_file):
     '''
@@ -44,7 +94,7 @@ def file_open_and_copy_text_function(js_file):
     cur_file.close()
     return functions
 
-def make_analyse_file(location, name, text):
+def make_output_files(location, name, text):
     '''
         Creates files from output of the website
         Args: location: The place to save the output file to
@@ -53,9 +103,33 @@ def make_analyse_file(location, name, text):
         Returns: Done or Errorcode
     '''
     filename = location+name
-    results = open(filename, "a")
-    results.write(text)
+    with open(filename, "w") as results:
+        json.dump(text, results, indent=4, sort_keys=True)
     results.close()
+    return 1
+
+def create_results(from_w_files, location, fname):
+    '''
+        Checks all the results for duplicates and saves in a new file without them
+        Args: from_w_files: where to look for JSON files to check for duplicates
+              location: Where to save the result file
+              fname: What to save result file
+    '''
+    content_result_file = {}
+    for root, dirs, files in os.walk(from_w_files, topdown=True, followlinks=False):
+        print(files)
+        for name in files:
+            if name.endswith('.JSON'):
+                filepath = ''.join((root, name))
+                with open(filepath, "r") as input_file:
+                    data = json.load(input_file)
+                for index, key in enumerate(data):
+                    value = list(data.values())
+                    already_known_functions = content_result_file.keys()
+                    if key not in already_known_functions:
+                        content_result_file[key] = value[index]
+    with open(location+"/"+fname, "w") as result:
+        json.dump(content_result_file, result, indent=2, sort_keys=True)
     return 1
 
 def main():
@@ -63,7 +137,11 @@ def main():
     parser.add_argument("location", help="Where are the files to check. Absolute path needed")
     args = parser.parse_args()
 
+    results_folder_path = create_result_folder(APP_MAIN_FOLDER)
+    print(results_folder_path)
+
     for js_file in check_js_files(args.location):
+        non_compatiblities = {}
         file_content = file_open_and_copy_text_function(js_file)
         browser = webdriver.Chrome()
         browser.get(URL)
@@ -77,18 +155,22 @@ def main():
         functions = browser.find_elements_by_xpath("//section[@class='report-section']")
         for function in functions:
             function_name = function.find_element_by_xpath("h3/a")
-            checked_function = "\n"+function_name.text+"\n\n"
-            make_analyse_file(RESULTS,"asd.txt",checked_function)
-            print(checked_function)
+            checked_function = function_name.text
             function_no_support = function.find_elements_by_xpath("section[@data-support='n']/ol/li")
+            browsers = []
             for browser_name in function_no_support:
-                not_supported_browser = browser_name.text+"\n"
-                print(not_supported_browser)
-                make_analyse_file(RESULTS,"asd.txt",not_supported_browser)
-        time.sleep(5)
+                if browser_name.text:
+                    browsers.append(browser_name.text)
+                non_compatiblities[checked_function]=browsers
+        # pp = pprint.PrettyPrinter(indent=1)
+        # pp.pprint(dict(non_compatiblities))
+        make_output_files(results_folder_path,filename(js_file, "Yes"),non_compatiblities)
+        time.sleep(1)
         browser.quit()
     #analyse all outputs and present in new file
-    #Delete temp files
+    final_result_folder = results_folder_path+"/Result"
+    os.mkdir(final_result_folder)
+    create_results(results_folder_path,final_result_folder, "result.JSON")
 
 if __name__ == '__main__':
   main()
